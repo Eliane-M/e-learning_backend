1aa058ace6157493390276839ca085320c97c799
Eliane_M <munezeroeliane761@gmail.com>
2024-08-27 09:58:40 +0200

    added localhost to allowed hosts

--- e_learning/e_learning/settings.py
+++ e_learning/e_learning/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", 'localhost', 'https://e-learning-backend-lf2d.onrender.com/']
+ALLOWED_HOSTS = ["127.0.0.1", 'localhost', 'e-learning-backend-lf2d.onrender.com']
 
 
 # Application definition
